61b09faccac76a6548fa2096ea6fb39bf8bc64d4
blanco <dehara14@gmail.com>
2024-03-13 14:01:24 -0600

    empezado formulario login

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,23 @@
+import type { FC } from "react";
+import Img from "./Img";
+import LogoCA from "/logoCA.png?url";
+
+const LoginForm: FC = () => {
+  return (
+    <section className="flex justify-evenly items-center">
+      <form className="h-full shadow-lg p-2   rounded-lg flex flex-col">
+        <Img source={LogoCA} alt="LogoCA" />
+        <div>
+          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Nombre de usuario
+          </label>
+          <input
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+export default LoginForm;

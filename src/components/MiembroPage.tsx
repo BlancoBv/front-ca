4b2cd7f6374cbb5fa624adef3e3377999904c041
Herrera-GG <dvdavefenix014@gmail.com>
2024-04-16 12:25:15 -0600

    CintaPage

--- src/components/MiembroPage.tsx
+++ src/components/MiembroPage.tsx
@@ -1,13 +1,12 @@
 import type { FC } from "react";
 import useFetchData from "../hooks/useFetchData";
 import Img from "./Img";
-import Tablespersonal from "./Tablespersonal";
 
 const MiembroPage: FC<{ id: string | undefined }> = ({ id }) => {
   const { data, isPending, error } = useFetchData("/configs/members.json");
 
   return (
-    <section className="flex flex-col px-40">
+    <section className=" flex-wrap mx-32 my-12">
       {isPending && <div>Cargando...</div>}
       {!isPending && !error && <Success data={data} id={id} />}
     </section>
@@ -24,22 +23,13 @@ const Success: FC<{ data: any; id: any }> = ({ data, id }) => {
   console.log(filteredMember);
   return (
     <>
-      <div className="flex w-full h-28 items-center justify-between">
-        <h1>{filteredMember.nombre}</h1>
-        <ul className="list-none flex">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>Lider CA</li>
-        </ul>
-      </div>
-      <div className="flex-wrap">
+      <div className="flex-wrap flex-col">
         <Img
           source={filteredMember.img}
           alt={filteredMember.nombre}
           styles="size-40 rounded-lg"
         />
-        <div className="flex flex-col min-w-24 pl-8 space-y-4">
+        <div className="flex flex-col min-w-full pl-8 space-y-4">
           <div className="flex flex-col border-b-2 border-b-gray-300 py-2">
             <h2 className="">{filteredMember.nombre}</h2>
             <h3 className="">{filteredMember.puesto}</h3>
@@ -53,7 +43,7 @@ const Success: FC<{ data: any; id: any }> = ({ data, id }) => {
             </a>
           </div>
           <div className="flex">
-            <p className="text-justify flex-grow p-1">
+            <p className="text-justify flex-wrap min-w-32">
               {filteredMember.resumen}
             </p>
           </div>

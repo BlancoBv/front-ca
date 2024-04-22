380edc13bb1ea2f3b83521a90e47a46e52629f32
blanco <dehara14@gmail.com>
2024-04-22 16:53:25 -0600

    cambiado api a privada

--- src/utils/prismicFetch.ts
+++ src/utils/prismicFetch.ts
@@ -4,6 +4,8 @@ const API_ENDPOINT: string = import.meta.env.PUBLIC_PRISMIC_API_URL;
 
 const client = prismic.createClient(API_ENDPOINT, {
   routes: [{ type: "post", path: "/:uid" }],
+  accessToken:
+    "MC5aaUd1RFJBQUFDVUFRcWpU.axp377-977-977-977-977-9V1fvv70d77-977-9UO-_ve-_vUrvv70FYm0v77-9el7vv73vv73vv70yVu-_vQ",
 });
 
 export const getPost = () => {

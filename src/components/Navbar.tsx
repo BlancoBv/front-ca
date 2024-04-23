342f3ead57280d0da98e4ba6b0e022aadbf31ed1
blanco <dehara14@gmail.com>
2024-04-22 21:55:44 -0600

    barra de navegcion superior y prinicipal responsivas

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
+import type { FC } from "react";
 import useGetData from "../hooks/useGetData";
 import Menudropdown from "./Menudropdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
 
 interface info {
   id: number;
@@ -13,35 +16,53 @@ interface info {
   }[];
 }
 
-const Navbar = () => {
+const Navbar: FC<{ pathname: string }> = ({ pathname }) => {
   const { data, isPending } = useGetData("/menuAzul");
 
-  const { pathname } = window.location;
-
-  console.log(data);
+  const showMenu = () => {
+    const element = document.getElementById("container-nav");
+    element?.classList.remove("make-invisible");
+    element?.classList.add("make-visible");
+  };
+  const hideMenu = () => {
+    const element = document.getElementById("container-nav");
+    element?.classList.remove("make-visible");
+    element?.classList.add("make-invisible");
+  };
 
   return (
-    <nav className="flex-grow flex flex-row justify-evenly">
-      {!isPending &&
-        data.map((value: info) =>
-          value.SubMenus.length > 0 ? (
-            <Menudropdown
-              name={value.nombre}
-              items={value.SubMenus}
-              test={value.url}
-              baseUrl={value.url}
-            />
-          ) : (
-            <a
-              href={`/${value.url}`}
-              className={`border-blue-600 ${
-                pathname === value.url ? "border-b-2 text-blue-600" : ""
-              }`}
-            >
-              {value.nombre}
-            </a>
-          )
-        )}
+    <nav className="flex-grow flex flex-row justify-evenly main-nav">
+      <FontAwesomeIcon
+        className="absolute right-2"
+        icon={faBars}
+        onClick={showMenu}
+      />
+      <div
+        id="container-nav"
+        className="w-full flex justify-evenly make-invisible"
+      >
+        <FontAwesomeIcon icon={faBarsStaggered} onClick={hideMenu} />
+        {!isPending &&
+          data.map((value: info) =>
+            value.SubMenus.length > 0 ? (
+              <Menudropdown
+                name={value.nombre}
+                items={value.SubMenus}
+                test={value.url}
+                baseUrl={value.url}
+              />
+            ) : (
+              <a
+                href={`/${value.url}`}
+                className={`border-blue-600 ${
+                  pathname === value.url ? "border-b-2 text-blue-600" : ""
+                }`}
+              >
+                {value.nombre}
+              </a>
+            )
+          )}
+      </div>
     </nav>
   );
 };
